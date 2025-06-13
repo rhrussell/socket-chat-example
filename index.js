7cd153bf8908f9730935c8b5d8ad1e93b0f5a7cf
Ryan Russell <ryan.russell@aa.com>
2025-06-13 17:23:09 -0500

    Step #5 Broadcasting

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
